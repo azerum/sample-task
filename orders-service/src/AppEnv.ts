049158997c728c0bf86155005c43d4b4ee0211e3
azerum <60925680+azerum@users.noreply.github.com>
2025-10-25 00:36:59 +0200

    feat(orders-service): defaults for env

--- orders-service/src/AppEnv.ts
+++ orders-service/src/AppEnv.ts
@@ -1,8 +1,15 @@
 import { type EnvSchema, type EnvType } from 'ts-dotenv'
 
 export const appEnvSchema = {
-    RABBITMQ_URL: String,
-    MONGODB_URL: String,
+    RABBITMQ_URL: {
+        type: String,
+        default: 'amqp://guest:guest@localhost:5672'
+    },
+
+    MONGODB_URL: {
+        type: String,
+        default: 'mongodb://localhost:27017'
+    },
 } satisfies EnvSchema
 
 export type AppEnv = EnvType<typeof appEnvSchema>
